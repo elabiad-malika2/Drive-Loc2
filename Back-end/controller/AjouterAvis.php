e3ecef0d2c5ae663504af16ea7e8b41d331f69c6
Malika <elabiadmalika64@gmail.com>
2025-01-04 20:30:32 +0100

    Ajouter de fonctionnaliter d'ajouter une avis

---
+++ Back-end/controller/AjouterAvis.php
@@ -0,0 +1,19 @@
+<?php
+require_once('../Model/avis.php');
+require_once('../Model/database.php');
+
+if($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['submit'])){
+        $message = trim(htmlspecialchars($_POST['message']));
+        $stars = trim(htmlspecialchars($_POST['stars']));
+        $reservationId = trim(htmlspecialchars($_POST['reservation_id']));
+        $database = new GestionBaseDeDonnees();
+        $pdo = $database->getConnection();
+
+        $avis = new Avis(null,$message,$stars,$reservationId);
+        $resultat = $avis->ajouterAvis($pdo);
+        if ($resultat == 'OK') {
+            header('Location: ../../front-end/client/reservation.php')
+        }
+
+}
+?>
